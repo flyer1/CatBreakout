1f3facc54f60148467c764552a044d5a462fb73b
flyer1 <jon.kroeker2@gmail.com>
2020-08-19 21:29:45 -0500

    Added filter/shadow and text to leaf nodes.

--- src/app/covid-tracker/home/covid-tracker.component.ts
+++ src/app/covid-tracker/home/covid-tracker.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { BaseType, Selection, select } from 'd3-selection';
-import { ScaleSequential, scaleSequential } from 'd3-scale';
-import { interpolateMagma } from 'd3-scale-chromatic';
+import { ScaleSequential, scaleSequential, scaleLinear } from 'd3-scale';
+import { interpolateMagma, interpolateGnBu, interpolateYlGn, interpolateSinebow, interpolateOrRd, interpolateBuPu, interpolateInferno, interpolateCubehelixDefault, interpolateRainbow } from 'd3-scale-chromatic';
 import { nest } from 'd3-collection';
+import { interpolateHcl } from 'd3-interpolate';
 
 import { pack, hierarchy, HierarchyCircularNode } from 'd3-hierarchy';
 import { SchoolService } from '../services/school.service';
 
 @Component({
   templateUrl: './covid-tracker.component.html',
-  styleUrls: ['./covid-tracker.component.scss']
+  styleUrls: ['./covid-tracker.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class CovidTrackerComponent implements OnInit {
 
@@ -46,17 +48,31 @@ export class CovidTrackerComponent implements OnInit {
     // this.clearSvg();
 
     this.colorScale = scaleSequential(interpolateMagma).domain([8, 0]);
+    // this.colorScale = scaleLinear()
+    //   .domain([0, 5])
+    //   .range(['hsl(152,80%,80%)', 'hsl(228,30%,40%)'])
+    //   .interpolate(interpolateHcl)
+    // this.colorScale = scaleSequential(interpolateRainbow).domain([5, 0]);
 
     // this.width = +this.el.nativeElement.offsetWidth;
     this.createSvg();
 
     console.log(nest().key((d: any) => d.height).entries(this.root.descendants()));
 
+    // Add some shadows
+    this.svg.append('filter')
+      .attr('id', 'shadow')
+      .append('feDropShadow')
+      .attr('flood-opacity', 0.3)
+      .attr('dx', 0)
+      .attr('dy', 1);
+
+    // Render the circles from the 4 levels of data (school/cohort/class/student)
     const node = this.g.
       selectAll('g')
       .data(nest().key((d: any) => d.height).entries(this.root.descendants()))
       .join('g')
-      // .attr('filter', shadow)
+      .attr('filter', `url('#shadow')`)
       .attr('class', 'test')
       .selectAll('g')
       .data((d: any) => d.values)
@@ -67,10 +83,28 @@ export class CovidTrackerComponent implements OnInit {
       .attr('r', (d: any) => d.r)
       .attr('fill', (d: any) => this.colorScale(d.height));
 
-    // this.initChartFunctions();
-    // this.renderChart();
 
-    // this.resize();
+    // This gets a bit messy but we need to create a clipping path for ever circle that has text inside.
+    // First make sure the id of each circle is the student's id (data.id)
+    const leaf = node.filter((d: any) => !d.children);
+
+    leaf.select('circle')
+      .attr('id', (d: any) => 'circle-' + d.data.data.id);
+
+    // Next add a clipPath for every circle and point to the circle that will contain the text
+    leaf.append('clipPath')
+      .attr('id', (d: any) => 'clip-' + d.data.data.id)
+      .append('use')
+      .attr('xlink:href', (d: any) => '#circle-' + d.data.data.id);
+
+    // Lastly add the text that will make use of the clipPath so we don't have text overflowing all over the place.
+    // So point at the clipPath which in turn knows the path to clip b/c it's referring to the circle. 
+    leaf.append('text')
+      .attr('clip-path', (d: any) => 'url(#clip-' + d.data.data.id + ')')
+      .attr('x', 0)
+      .attr('y', 3)
+      .text((d: any) => d.data.data.name);
+      
   }
 
   createSvg() {
@@ -90,14 +124,6 @@ export class CovidTrackerComponent implements OnInit {
     //   .attr('transform', `translate(${leftOffset}, ${topOffset})`);
   }
 
-  initChartFunctions() {
-    // scaleSequential([8,0], interpolateMagma)
-    const packer = pack().size([this.width, this.height]).padding(3);
-    const h = hierarchy
-    // packer(h);
-
-  }
-
   /** Return the function that takes in our raw data and returns back a hierarchical structure that has all of the information required to drive the visualization */
   createPackFunction() {
     return (sourceData: any) => pack()
@@ -107,10 +133,6 @@ export class CovidTrackerComponent implements OnInit {
         .sum(d => d.value)
         .sort((a: any, b: any) => b.name - a.name));
   }
-
-
-
-  renderChart() { }
 
   stratifyData() {
     const result = {
